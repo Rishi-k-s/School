17992e3d0f7c2b7b6648658edbe0f06a313d831f
Rishi-k-s <rishikrishna.sr@gmail.com>
2022-05-11 22:23:07 +0530

    divisibility

--- TestCodes/functions.py
+++ TestCodes/functions.py
@@ -1,4 +1,4 @@
-def addSubMulDiv(num1,num2):
+"""def addSubMulDiv(num1,num2):
     add = num1+num2
     sub = num1-num2
     mul = num1*num2
@@ -6,4 +6,17 @@ def addSubMulDiv(num1,num2):
     return(add,sub,mul,divd)
 addNum,subNum,multNum,divNum = addSubMulDiv(10,20)
 print(addNum,subNum,multNum,divNum)
+"""
+def Revert(Num,Last=2):
+    if Last%2==0:
+        Last=Last+1
+    else:
+        Last=Last-1
+    for C in range(1,Last+1):
+        Num+=C
+    print(Num)
+A,B=20,4
+Revert(A,B)
+B=B-1
+Revert(B)
 
