4eafe8c9f4cc6d9c54c4945cd9a3755f512741b4
Rishi-k-s <rishikrishna.sr@gmail.com>
2022-05-11 19:58:43 +0530

    sui

--- TestCodes/functions.py
+++ TestCodes/functions.py
@@ -1,3 +1,9 @@
-def test(*testlen):
-    return(type(testlen))
-print(test(10,20,30,40))
+def addSubMulDiv(num1,num2):
+    add = num1+num2
+    sub = num1-num2
+    mul = num1*num2
+    divd = num1/num2
+    return(add,sub,mul,divd)
+addNum,subNum,multNum,divNum = addSubMulDiv(10,20)
+print(addNum,subNum,multNum,divNum)
+
