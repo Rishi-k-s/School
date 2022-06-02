1cc23615824d6499d411e114b46e8a7ed9079b14
Rishi-k-s <rishikrishna.sr@gmail.com>
2022-06-02 22:26:13 +0530

    exam 2

---
+++ Exam/functions.py
@@ -0,0 +1,11 @@
+
+def facto():
+    x= int (input('Entwr a number:')) # 10
+    i = 1
+    count =1
+    while i<= x:
+        count *= i
+        i+=1
+    print(count)
+
+facto()
---
+++ Exam/test.py
@@ -0,0 +1,31 @@
+import math
+
+num1 = 10
+print(math.sqrt(num1))
+
+
+"""
+Built in func
+-----------------------
+str()
+float()
+eval()
+abs()
+type()
+round()
+range()
+
+
+MOdules
+--------------
+modulename.modulefunc(....)
+math:
+    sqrt()
+    pow()
+    floor()
+    ceil()
+
+"""
+
+
+
