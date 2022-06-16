b5b20470136661904c0da66e3edf12f2a75d9b69
Rishi-k-s <rishikrishna.sr@gmail.com>
2022-06-16 21:39:38 +0530

    tect func updated

--- File Operations (CH2)/textFunc.py
+++ File Operations (CH2)/textFunc.py
@@ -1,18 +1,26 @@
 
-file_handle = open("D:\School\File Operations (CH2)\school_Dat.txt", "w+")
-askDetails = ['School Name: ','Address: ',"phone no: ","email: "]
-while True:
-    try:
-        getNumOfTimesToAddData = int(input('Enter the no. of times th evalue should be added:'))
-    except:
-        print("It should be a number")
-        getNumOfTimesToAddData = int(input('Enter the no. of times th evalue should be added:'))
-    for appendData in range (getNumOfTimesToAddData):
-        getSchoolName = input(askDetails[0])
-        getAddress = input(askDetails[1])
-        getPhNo = input(askDetails[2])
-        getEmail = input(askDetails[3])
-        fullDetails = ['{} \n {}\n {} \n {}'.format(getSchoolName,getAddress,getPhNo,getEmail)]
-        file_handle.writelines(fullDetails)
-    break
-
+with open("D:\School\File Operations (CH2)\school_Dat.txt", "r+") as file_handle:
+    askDetails = ['School Name: ','Address: ',"phone no: ","email: "]
+    askNumCheck = True
+    while True:
+        while askNumCheck:
+            try:
+                getNumOfTimesToAddData = int(input('Enter the no. of times th evalue should be added:'))
+                askNumCheck = False
+            except:
+                print("It should be a number")
+                askNumhkeck = True
+        for appendData in range (getNumOfTimesToAddData):
+            getSchoolName = input(askDetails[0])
+            getAddress = input(askDetails[1])
+            getPhNo = input(askDetails[2])
+            getEmail = input(askDetails[3])
+            fullDetails = ['{}\n{}\n{}\n{}'.format(getSchoolName,getAddress,getPhNo,getEmail)]
+            file_handle.writelines(fullDetails)
+        wantToRead = input('Want to Read (yes/no):')
+        if wantToRead == 'yes':
+            for lines in file_handle:
+                print(file_handle.read())
+            break
+        else:
+            break
