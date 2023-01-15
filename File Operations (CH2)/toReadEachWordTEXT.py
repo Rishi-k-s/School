c42431b53b76c871e43da08bd1479c1249f68b65
Rishi-k-s <rishikrishna.sr@gmail.com>
2023-01-15 11:27:21 +0530

    testing

--- File Operations (CH2)/toReadEachWordTEXT.py
+++ File Operations (CH2)/toReadEachWordTEXT.py
@@ -1,15 +1,13 @@
-import pickle
+import csv
 
-file_handlr = open("D:\Dev\python\Testing_School\File Operations (CH2)\oomidat.dat","rb")
 
-try:
-    for i in file_handlr:
-        print(i)
-        print(pickle.load(file_handlr))
-except Exception as e:
-    print(e)
-# for each_line in line_list:
-#     seperateEachWords = each_line.split()
-#     for each_word in seperateEachWords:
-#         print('{} # '.format(each_word), end='')
+file_handlr = open("D:\Dev\python\Testing_School\File Operations (CH2)\gucci.csv","r",newline="\n")
+
+wrriter_obj = csv.reader(file_handlr)
+print(file_handlr.seek(30))
+for i in wrriter_obj:
+    print(i)
+print(file_handlr.tell())
 file_handlr.close()
+    
+
